2b3bd10a0e8bdc56268caff36a5d85fe961bf272
borisovboris <boris.s.borisov99@gmail.com>
2020-12-21 18:21:34 +0300

    fixed database driver

--- src/general/DBConnection.java
+++ src/general/DBConnection.java
@@ -7,19 +7,33 @@ import java.sql.Statement;
 
 public class DBConnection {
 	
-	public void connect() throws Exception {
+	Connection connection = null;
+	
+	public Connection getConnection() throws Exception {
 		
 		try {
 			Class.forName("org.sqlite.JDBC");
-		    Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
+		    connection = DriverManager.getConnection("jdbc:sqlite:test.db");
 			System.out.println("SQLite DB connected");	
+			
+//	        Statement statement = connection.createStatement();
+//	        statement.setQueryTimeout(30);  // set timeout to 30 sec.	
+//	        statement.executeUpdate("drop table if exists person");
+//	        statement.executeUpdate("create table person (id integer, name string)");
+//	        statement.executeUpdate("insert into person values(1, 'leo')");
+//	        statement.executeUpdate("insert into person values(2, 'yui')");
+//	        ResultSet rs = statement.executeQuery("select * from person");
+//	        while(rs.next())
+//	        {
+//	           // read the result set
+//	           System.out.println("name = " + rs.getString("name"));
+//	           System.out.println("id = " + rs.getInt("id"));
+//	        }
 		} catch(Exception e) { 
 			System.out.println(e);
-			System.out.println("AAAAAA");
 		}
-		
-		
-		
+			
+		return connection;
 	}
 
 }
